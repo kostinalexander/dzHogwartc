18b4f0f28955d3de3d46d5fdc803ec272b51b5d8
Александр <egorrak3221@mail.ru>
2023-07-20 15:07:40 +0300

    Сделал Дз

--- src/main/java/com/example/Dz22/repository/AvatarRepository.java
+++ src/main/java/com/example/Dz22/repository/AvatarRepository.java
@@ -1,12 +1,13 @@
 package com.example.Dz22.repository;
 
 import com.example.Dz22.model.Avatar;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface AvatarRepository extends PagingAndSortingRepository<Avatar,Long> {
+public interface AvatarRepository extends JpaRepository<Avatar,Long> {
     Optional<Avatar> findByStudentId(Long studentId);
     List<Avatar> findAll();
 }
